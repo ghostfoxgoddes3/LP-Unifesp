5606c1515962d92ab66096692680e55e60a01c5b
Gabriely Di Folco Rocha <gabydifolco@gmail.com>
2025-04-24 23:58:41 -0300

    Create ex12.c

---
+++ ex12.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(){
+  float salary, readjustment;
+  
+  printf("Qual o salário atual do funcionário?");
+  scanf("%f",&salary);
+  printf("Qual o percentual de reajuste?");
+  scanf("%f",&readjustment);
+
+  salary = salary*readjustment/100
+  
+  printf("Salário reajustado para o ano corrente: %.2f%%\n", salary);
+}
